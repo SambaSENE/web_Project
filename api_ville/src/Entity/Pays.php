d3f71b8f7f4c530b550f2f397708f5f3dfb72f17
Samba SENE <80283818+SambaSENE@users.noreply.github.com>
2024-02-20 12:56:14 +0100

    add

---
+++ api_ville/src/Entity/Pays.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\PaysRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+
+#[ORM\Entity(repositoryClass: PaysRepository::class)]
+#[ApiResource]
+class Pays
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 2)]
+    #[SerializedName('country_code')]
+    private ?string $code_pays = null;
+    
+    #[ORM\Column(length: 255)]
+    #[SerializedName('country_name')]
+    private ?string $nom_pays = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCodePays(): ?string
+    {
+        return $this->code_pays;
+    }
+
+    public function setCodePays(string $code_pays): static
+    {
+        $this->code_pays = $code_pays;
+
+        return $this;
+    }
+
+    public function getNomPays(): ?string
+    {
+        return $this->nom_pays;
+    }
+
+    public function setNomPays(string $nom_pays): static
+    {
+        $this->nom_pays = $nom_pays;
+
+        return $this;
+    }
+}
