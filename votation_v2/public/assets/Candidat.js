7fa88cb17f171ebedb1e3b8dfee3512bfd70c36e
samba sene <80283818+SambaSENE@users.noreply.github.com>
2024-04-14 20:35:32 +0200

    all

--- votation_v2/public/assets/Candidat.js
+++
@@ -1,8 +0,0 @@
-class Candidats
-{
-    constructor (_candidat){
-        Object.assign(_candidat)
-        this.urlPic = "'./assets/candidats/' + _candidat.id + 'jpg'";
-    } 
-}
-export { Candidats }
